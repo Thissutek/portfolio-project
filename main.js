de42446eddd68103941ed0af1c6cb653fccb56f1
Jonathan Yau <jonathan_yau@outlook.com>
2023-02-18 02:12:55 -0500

    finished card hover effect changed the logic behind js

--- main.js
+++ main.js
@@ -1,14 +1,10 @@
-const handleOnMouseMove = e => {
-    const {currentTarget: target} = e;
+document.getElementById("cards").onmousemove = e => {
+    for(const card of document.getElementsByClassName("card")) {
+    const rect = card.getBoundingClientRect(),
+        x = e.clientX - rect.left,
+        y = e.clientY - rect.top;
 
-    const rect = target.getBoundingClientRect(),
-    x = e.clientX - rect.left,
-    y = e.clientY - rect.top;
-
-    target.style.setProperty("--mouse-x", `${x}px`);
-    target.style.setProperty("--mouse-y", `${y}px`);
-}
-
-for(const card of document.querySelectorAll(".card")) {
-    card.onmousemove = e => handleOnMouseMove(e);
+    card.style.setProperty("--mouse-x", `${x}px`);
+    card.style.setProperty("--mouse-y", `${y}px`);
+    }
 }
