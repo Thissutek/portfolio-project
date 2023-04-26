9d20e467ccf4a209913c49f48c5cd899610aa961
Jonathan Yau <jonathan_yau@outlook.com>
2023-04-26 19:25:48 -0400

    fixed the javascript for the backlight that was highlighting only the first section and not working in new sections I added to HTML

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-document.getElementById("cards").onmousemove = e => {
+/*document.getElementById("cards").onmousemove = e => {
     for(const card of document.getElementsByClassName("card")) {
     const rect = card.getBoundingClientRect(),
         x = e.clientX - rect.left,
@@ -17,3 +17,22 @@ document.body.onpointermove = event => {
     backlight.style.left = `${clientX}px`;
     backlight.style.top = `${clientY}px`;
 }
+*/
+
+document.querySelectorAll(".card").forEach(card => {
+    card.addEventListener("mousemove", e => {
+      const rect = card.getBoundingClientRect();
+      const x = e.clientX - rect.left;
+      const y = e.clientY - rect.top;
+  
+      card.style.setProperty("--mouse-x", `${x}px`);
+      card.style.setProperty("--mouse-y", `${y}px`);
+    });
+  });
+  
+  const backlight = document.getElementById("backlight");
+  
+  document.body.addEventListener("pointermove", event => {
+    backlight.style.left = `${event.clientX}px`;
+    backlight.style.top = `${event.clientY}px`;
+  });
