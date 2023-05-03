cca2dee96030a2d47c88a41490c652b03c3b6e72
Jonathan Yau <jonathan_yau@outlook.com>
2023-05-03 16:42:53 -0400

    Added a parallax feature for the About Me page, I dont't like how the text is also parallaxed but when the image is only parallaxed it doesn't make sense for the next commit reorganize the About me Page to make the parallax work better

--- bio/secondary.js
+++ bio/secondary.js
@@ -10,3 +10,13 @@ document.body.onpointermove = event => {
     
 }
 */
+
+const parallaxImg = document.querySelector(".parallax-img");
+const parallaxText = document.querySelector(".bio-description");
+
+document.addEventListener("mousemove", (e) => {
+  const x = e.clientX / window.innerWidth;
+  const y = e.clientY / window.innerHeight;
+  parallaxImg.style.transform = `translate(-${x * 25}px, -${y * 25}px)`;
+  parallaxText.style.transform = `translate(-${x * 25}px, -${y * 25}px)`;
+});
