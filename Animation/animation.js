a23bef17d90fc8c6be32a34f2f2ffc2d5f442197
Jonathan Yau <jonathan_yau@outlook.com>
2023-05-31 16:28:22 -0400

    Updated all Gifs into video files with a hover to play feature

--- Animation/animation.js
+++ Animation/animation.js
@@ -6,3 +6,18 @@ gifs.forEach((gif) => {
         gif.src = gif.src;
     });
 }); */
+
+//Gets all the video elements
+const videos = document.querySelectorAll('.grid-item video');
+
+// Event listeners for mouse hover
+videos.forEach(video => {
+    video.addEventListener('mouseover', () => {
+        video.play();
+    });
+
+    video.addEventListener('mouseout', () => {
+    video.pause();
+    });
+});
+
