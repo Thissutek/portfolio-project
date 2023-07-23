f5fc872dec4c2ce6c7e612bc0bb75d86777a3717
Jonathan Yau <jonathan_yau@outlook.com>
2023-07-23 19:29:26 -0400

    Fixed carousel to be seperate functions

--- Ux-Ui Projects/Project Homework/project-homework.js
+++ Ux-Ui Projects/Project Homework/project-homework.js
@@ -1,5 +1,5 @@
-let currentSlide = 0;
-const slides = document.querySelectorAll('.slide');
+/*let currentSlide = 0;
+const slides = document.querySelectorAll('.slide-1');
 
 function showSlide(slideIndex) {
     slides.forEach((slide, index) => {
@@ -27,3 +27,57 @@ function nextSlide() {
 
 // Show the first slide initially
 showSlide(currentSlide);
+
+*/
+
+let currentSlide1 = 0;
+const slides1 = document.querySelectorAll('.slide-1');
+
+let currentSlide2 = 0;
+const slides2 = document.querySelectorAll('.slide-2');
+
+function showSlide(slideIndex, slides) {
+  slides.forEach((slide ,index) => {
+    const offset = (index - slideIndex) * 100;
+    slide.style.transform = `translateX(${offset}%)`;
+  });
+}
+
+function prevSlide(slides, currentSlide) {
+  let prevSlideIndex = currentSlide - 1;
+  if (prevSlideIndex < 0) {
+    prevSlideIndex = slides.length -1;
+  }
+  showSlide(prevSlideIndex, slides);
+  return prevSlideIndex;
+}
+
+function nextSlide(slides, currentSlide) {
+  let nextSlideIndex = currentSlide + 1;
+  if (nextSlideIndex >= slides.length) {
+    nextSlideIndex = 0;
+  }
+  showSlide(nextSlideIndex, slides);
+  return nextSlideIndex;
+}
+
+//Show the first slide for each carousel initially 
+showSlide(currentSlide1, slides1);
+showSlide(currentSlide2, slides2);
+
+//Event Listeners for prev and next buttons of each carousel 
+document.querySelector('.carousel-1 .prev-btn').addEventListener('click', () => {
+  currentSlide1 = prevSlide(slides1, currentSlide1);
+});
+
+document.querySelector('.carousel-1 .next-btn').addEventListener('click', () => {
+  currentSlide1 = nextSlide(slides1, currentSlide1);
+});
+
+document.querySelector('.carousel-2 .prev-btn').addEventListener('click', () => {
+  currentSlide2 = prevSlide(slides2, currentSlide2);
+});
+
+document.querySelector('.carousel-2 .next-btn').addEventListener('click', () => {
+  currentSlide2 = nextSlide(slides2, currentSlide2)
+})
