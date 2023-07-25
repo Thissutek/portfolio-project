67ea05cf2a0f2d9a3c592f1116a936a014b42502
Jonathan Yau <jonathan_yau@outlook.com>
2023-07-25 02:54:42 -0400

    Found the reason why the carousel was missing one image need to fix the css or the actual function

--- Ux-Ui Projects/Project Homework/project-homework.js
+++ Ux-Ui Projects/Project Homework/project-homework.js
@@ -1,35 +1,3 @@
-/*let currentSlide = 0;
-const slides = document.querySelectorAll('.slide-1');
-
-function showSlide(slideIndex) {
-    slides.forEach((slide, index) => {
-    const offset = (index - slideIndex) * 100;
-      slide.style.transform = `translateX(${offset}%)`;
-    });
-    currentSlide = slideIndex;
-  }
-
-function prevSlide() {
-    let prevSlideIndex = currentSlide - 1;
-    if (prevSlideIndex < 0) {
-      prevSlideIndex = slides.length - 1;
-    }
-    showSlide(prevSlideIndex);
-  }
-
-function nextSlide() {
-    let nextSlideIndex = currentSlide + 1;
-    if (nextSlideIndex >= slides.length) {
-      nextSlideIndex = 0;
-    }
-    showSlide(nextSlideIndex);
-  }
-
-// Show the first slide initially
-showSlide(currentSlide);
-
-*/
-
 let currentSlide1 = 0;
 const slides1 = document.querySelectorAll('.slide-1');
 
